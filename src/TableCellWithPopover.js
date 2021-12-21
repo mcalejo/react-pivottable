0598a5254bffe1e20fee97a287c94f8d5e41387e
jacintodavila <>
2021-12-21 09:39:25 -0400

    Adding styling to popover's button

--- src/TableCellWithPopover.js
+++ src/TableCellWithPopover.js
@@ -2,11 +2,26 @@ import React from 'react';
 import Popover from '@mui/material/Popover';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { makeStyles } from '@mui/styles';
 import DisplayObject from './DisplayObject'
+
+// from https://mui.com/styles/basics/
+const useStyles = makeStyles({
+  root: {
+    background: 'white',
+    border: 0,
+    borderRadius: 1,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'black',
+    height: 24,
+    padding: '0 3px',
+  },
+});
 
 // based on https://mui.com/components/popover/
 
 const TableCellWithPopover = props => {
+  const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);  
   
   const handleClick = (event) => {
@@ -26,10 +41,14 @@ const TableCellWithPopover = props => {
   //const elements = props.content.map( el => 
   //  <li>{el}</li>
   //)
+
+  //  <Button aria-describedby={id} className={classes.root} variant="contained" onClick={handleClick}>
+  //{props.value}
+  //</Button>
   
   return (
     <div>
-      <Button aria-describedby={id} variant="contained" onClick={handleClick}>
+      <Button className={classes.root} onClick={handleClick}>
         {props.value}
       </Button>
       <Popover
